a3ef81346e851d01f714a4695bdd2d365c8f6c8d
renaruna <79509944+renaruna@users.noreply.github.com>
2021-11-29 22:09:18 +0900

    Update name.js

--- name.js
+++ name.js
@@ -1,18 +1,23 @@
 const outputElement = document.getElementById('output_csv');
 
+//CSVファイルを読み込む
 function getCsvData(dataPath) {
-    const request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();// HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    
+    // レスポンスが返ってきたらconvertArray()を呼ぶ	
     request.addEventListener('load', (event) => {
      const response = event.target.responseText;
      convertArray(response);
     });
     request.open('GET', dataPath, true);
-    request.send();
+    request.send();// HTTPリクエストの発行
 }
    
 function convertArray(data) {
     const dataArray = [];
-    const dataString = data.split('n');
+    const dataString = data.split('\n');// 改行を区切り文字として行を要素とした配列を生成
+    
+    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
     for (let i = 0; i < dataString.length; i++) {
      dataArray[i] = dataString[i].split(',');
     }
