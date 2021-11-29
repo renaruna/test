a94639e1a8adaef57ebbe31b6c004c9a9de71e40
renaruna <79509944+renaruna@users.noreply.github.com>
2021-11-29 22:00:51 +0900

    Update name.js

--- name.js
+++ name.js
@@ -1,13 +1,22 @@
 const outputElement = document.getElementById('output_csv');
 
 function getCsvData(dataPath) {
- const request = new XMLHttpRequest();
- request.addEventListener('load', (event) => {
-  const response = event.target.responseText;
-  outputElement.innerHTML = response;
- });
- request.open('GET', dataPath, true);
- request.send();
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', (event) => {
+     const response = event.target.responseText;
+     convertArray(response);
+    });
+    request.open('GET', dataPath, true);
+    request.send();
 }
-
-getCsvData('index.csv');
+   
+function convertArray(data) {
+    const dataArray = [];
+    const dataString = data.split('n');
+    for (let i = 0; i < dataString.length; i++) {
+     dataArray[i] = dataString[i].split(',');
+    }
+    outputElement.innerHTML = dataArray;
+}
+   
+   getCsvData('index.csv');
