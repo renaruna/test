01a36798c95dcfb036981835c20eb9938d310f3a
renaruna <79509944+renaruna@users.noreply.github.com>
2021-11-30 09:28:57 +0900

    Update name.js

--- name.js
+++ name.js
@@ -15,30 +15,30 @@ function getCsvData(dataPath) {
    
 function convertArray(data) {
     var dataArray = [];
-    
+    var dataObject = {};
     const dataString = data.split('\n');// 改行を区切り文字として行を要素とした配列を生成
     
     // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
     for (let i = 0; i < dataString.length; i++) {
      dataArray[i] = dataString[i].split(',');
         if(i == 1) {
-            dataObject[i].id = dataArray[i][0];
-            dataObject[i].name = dataArray[i][1];
-            dataObject[i].maker = dataArray[i][2];
-            dataObject[i].url = dataArray[i][3];
-            dataObject[i].date = dataArray[i][4];
-            dataObject[i].KP = dataArray[i][5];
-            dataObject[i].driver = dataArray[i][6];
-            dataObject[i].maneger = dataArray[i][7];
-            dataObject[i].boss = dataArray[i][8];
-            dataObject[i].spiritual = dataArray[i][9];
-            dataObject[i].warcrim = dataArray[i][10];
-            dataObject[i].creater = dataArray[i][11];
+            dataObject.id = dataArray[i][0];
+            dataObject.name = dataArray[i][1];
+            dataObject.maker = dataArray[i][2];
+            dataObject.url = dataArray[i][3];
+            dataObject.date = dataArray[i][4];
+            dataObject.KP = dataArray[i][5];
+            dataObject.driver = dataArray[i][6];
+            dataObject.maneger = dataArray[i][7];
+            dataObject.boss = dataArray[i][8];
+            dataObject.spiritual = dataArray[i][9];
+            dataObject.warcrim = dataArray[i][10];
+            dataObject.creater = dataArray[i][11];
         }
     }
 
     outputElement.innerHTML = dataArray[2][1];
-    document.getElementById('maker').innerHTML = dataArray[3][1];
+    document.getElementById('maker').innerHTML = dataObject.maneger;
 
 }
    
