60bb6c5f0da40d885f38c799d570cb42d34335bf
renaruna <79509944+renaruna@users.noreply.github.com>
2021-11-30 09:27:06 +0900

    Update name.js

--- name.js
+++ name.js
@@ -21,7 +21,20 @@ function convertArray(data) {
     // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
     for (let i = 0; i < dataString.length; i++) {
      dataArray[i] = dataString[i].split(',');
-        
+        if(i == 1) {
+            dataObject[i].id = dataArray[i][0];
+            dataObject[i].name = dataArray[i][1];
+            dataObject[i].maker = dataArray[i][2];
+            dataObject[i].url = dataArray[i][3];
+            dataObject[i].date = dataArray[i][4];
+            dataObject[i].KP = dataArray[i][5];
+            dataObject[i].driver = dataArray[i][6];
+            dataObject[i].maneger = dataArray[i][7];
+            dataObject[i].boss = dataArray[i][8];
+            dataObject[i].spiritual = dataArray[i][9];
+            dataObject[i].warcrim = dataArray[i][10];
+            dataObject[i].creater = dataArray[i][11];
+        }
     }
 
     outputElement.innerHTML = dataArray[2][1];
