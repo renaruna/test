c26e5c8f1a11cf5b90a4ff0f5bd38d8728519db1
renaruna <79509944+renaruna@users.noreply.github.com>
2022-02-11 14:07:21 +0900

    芋づる関数

--- chara-manager2.js
+++ chara-manager2.js
@@ -27,73 +27,83 @@ const bossElement = document.getElementById('boss');
 const spiritualElement = document.getElementById('spiritual');
 const warcrimElement = document.getElementById('warcrim');
 const creatorElement = document.getElementById('creator');
-const dataArray = [];
+const cArray = [];
 const sArray = [];
 const dataObject = {};
 
 //CSVファイルを読み込む
-function getCsvData(dataPath[]) {
+function getCsvData(dataPathC, dataPathS) {
     const request = new XMLHttpRequest();// HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
     
     // レスポンスが返ってきたらconvertArray()を呼ぶ	
     request.addEventListener('load', (event) => {
      const response = event.target.responseText;
-     convertArray(response);
+     getCsvS(response, dataPathS);
     });
-    request.open('GET', dataPath, true);
+    request.open('GET', dataPathC, true);
     request.send();// HTTPリクエストの発行
-
-    
 
 }
    
-function convertArray(data[]) {
-    const dataString1 = data[0].split('\n');// 改行を区切り文字として行を要素とした配列を生成
-    const dataString2 = data[1].split('\n');
+function getCsvS(data, dataPathS) {
+    const request = new XMLHttpRequest();// HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    
+    // レスポンスが返ってきたらconvertArray()を呼ぶ	
+    request.addEventListener('load', (event) => {
+     const response = event.target.responseText;
+     convertArray(data, response);
+    });
+    request.open('GET', dataPathS, true);
+    request.send();// HTTPリクエストの発行
+}
+
+function convertArray(dataC, dataS) {
+    const dataStringC = dataC.split('\n');// 改行を区切り文字として行を要素とした配列を生成
+    const dataStringS = dataS.split('\n');
     
     // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
-    for (let i = 0; i < dataString1.length; i++) {
-     dataArray[i] = dataString1[i].split(',');
+    for (let i = 0; i < dataStringC.length; i++) {
+     cArray[i] = dataStringC[i].split(',');
     }
-    for (let i = 0; i < dataString2.length; i++) {
-     sArray[i] = dataString2[i].split(',');
+    for (let i = 0; i < dataStringS.length; i++) {
+     sArray[i] = dataStringS[i].split(',');
     }
-    for (let i = 0; i < dataString1.length; i++) {
+    for (let i = 0; i < dataStringC.length; i++) {
         if(i == charaID) {
-            dataObject.id = dataArray[i][0];
-            dataObject.name = dataArray[i][1];
-            dataObject.hiraname = dataArray[i][2];
-            dataObject.age = dataArray[i][3];
-            dataObject.sex = dataArray[i][4];
-            dataObject.job = dataArray[i][5];
+            dataObject.id = cArray[i][0];
+            dataObject.name = cArray[i][1];
+            dataObject.hiraname = cArray[i][2];
+            dataObject.age = cArray[i][3];
+            dataObject.sex = cArray[i][4];
+            dataObject.job = cArray[i][5];
             dataObject.member = "管理人";
-            dataObject.color = dataArray[i][6];
+            dataObject.color = cArray[i][6];
         
-            dataObject.hp = dataArray[i][7];
-            dataObject.mp = dataArray[i][8];
-            dataObject.san = dataArray[i][9];
-            dataObject.db = dataArray[i][10];
-            dataObject.str = dataArray[i][11];
-            dataObject.con = dataArray[i][12];
-            dataObject.pow = dataArray[i][13];
-            dataObject.dex = dataArray[i][14];
-            dataObject.app = dataArray[i][15];
-            dataObject.siz = dataArray[i][16];
-            dataObject.int = dataArray[i][17];
-            dataObject.edu = dataArray[i][18];
+            dataObject.hp = cArray[i][7];
+            dataObject.mp = cArray[i][8];
+            dataObject.san = cArray[i][9];
+            dataObject.db = cArray[i][10];
+            dataObject.str = cArray[i][11];
+            dataObject.con = cArray[i][12];
+            dataObject.pow = cArray[i][13];
+            dataObject.dex = cArray[i][14];
+            dataObject.app = cArray[i][15];
+            dataObject.siz = cArray[i][16];
+            dataObject.int = cArray[i][17];
+            dataObject.edu = cArray[i][18];
 
-            dataObject.skill = dataArray[i][19];
-            dataObject.setting = dataArray[i][20];
-            dataObject.scenario = dataArray[i][21];
+            dataObject.skill = cArray[i][19];
+            dataObject.setting = cArray[i][20];
+            dataObject.scenario = cArray[i][21];
         
-            dataObject.driverCom = dataArray[i][22];
-            dataObject.managerCom = dataArray[i][23];
-            dataObject.bossCom = dataArray[i][24];
-            dataObject.spiritualCom = dataArray[i][25];
-            dataObject.warcrimCom = dataArray[i][26];
-            dataObject.creatorCom = dataArray[i][27];
+            dataObject.driverCom = cArray[i][22];
+            dataObject.managerCom = cArray[i][23];
+            dataObject.bossCom = cArray[i][24];
+            dataObject.spiritualCom = cArray[i][25];
+            dataObject.warcrimCom = cArray[i][26];
+            dataObject.creatorCom = cArray[i][27];
             
-            dataObject.maker = dataArray[i][28];
+            dataObject.maker = cArray[i][28];
             break;
         }
     }
@@ -139,7 +149,7 @@ function display() {
 }
 
 function search(thing) {
-｝
+}
 
 function change(text, a, b) {
   let i = 0;
@@ -151,4 +161,3 @@ function change(text, a, b) {
 }
    
 getCsvData('character-index - manager.csv', 'website - scenario.csv');
-
