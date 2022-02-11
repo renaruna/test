b9083be21beb6d480153611f93bf36d178f4e3c9
renaruna <79509944+renaruna@users.noreply.github.com>
2022-02-12 03:33:11 +0900

    Update name.js

--- name.js
+++ name.js
@@ -267,10 +267,10 @@ function display() {
             }
 
             //HTML
-            text += '<div><h2>第'+(j+1)+'陣</h2></div><div class="item"><table><tr><th>プレイした日付</th><td><p>'+dataObject.date+'</p></td></tr><tr><th>KP</th><td><p><a href='+memberKP+'>'+dataObject.KP+'</a></p></td></tr><tr><th>CAST</th><td><div><p>PC</p>'+driver+manager+boss+spiritual+warcrim+creator+'</div></td></tr></table></div></div><div>';
+            text += '<h2>第'+(j+1)+'陣</h2></div><div>';
         }
 
-        text += '<div><h2>第'+(j+1)+'陣</h2>';
+        text += '<h2>第'+(jin+1)+'陣</h2>';
         jinElement.innerHTML = text;
 
         let dataObject = {};
