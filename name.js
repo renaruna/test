e79414686984771c19ef0ae3a77514a49c869bf2
renaruna <79509944+renaruna@users.noreply.github.com>
2022-02-12 16:11:59 +0900

    Update name.js

--- name.js
+++ name.js
@@ -251,7 +251,7 @@ function display() {
             }
 
             //HTML
-            text += '<h2>第'+(j+1)+'陣</h2></div><div class="item"><table><tr><th>プレイした日付</th><td><p>'+dataObject.date+'</p></td></tr><tr><th>KP</th><td><p>'+kpHtml+'</p></td></tr><tr><th>CAST</th><td><div><p>PC</p>'+driver+manager+boss+spiritual+warcrim+creator+'</div></td></tr></table></div><div>';
+            text += '<h2>第'+(j+1)+'陣</h2></div><div class="item"><table><tr><th>プレイした日付</th><td><p>'+dataObject.date+'</p></td></tr><tr><th>KP</th><td><p>'+'きーぱー'+'</p></td></tr><tr><th>CAST</th><td><div><p>PC</p>'+driver+manager+boss+spiritual+warcrim+creator+'</div></td></tr></table></div><div>';
         }
 
         text += '<h2>第'+jin+'陣</h2>';
