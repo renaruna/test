c29c0af3d77d3dc840bc5eaddb8dbd685d7d6892
renaruna <79509944+renaruna@users.noreply.github.com>
2022-03-01 01:31:08 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
 var times = 0;
-var rpt = document.getElementById("repeat-images");
-var dftr = rpt.src;
 function repeater(){
     times++;
     if(times % 2 !== 0) {
-        rpt.src = rpt.dataset.secondImage;
+        $('.rpt').show();
     } else {
-        rpt.src = dftr;
+        $('.rpt').hide();
     }
 }
