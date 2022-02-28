d65bfb2094fd1e37220975226b9b3976dce1c860
renaruna <79509944+renaruna@users.noreply.github.com>
2022-03-01 01:55:02 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 var times = 0;
+var $home = $('.rpt');
+
 function repeater(){
     times++;
     if(times % 2 !== 0) {
-        $('.rpt').show();
+         $home.animate({'marginRight':'0'},500); 
     } else {
-        $('.rpt').hide();
+         $home.animate({'marginRight':'380px'},500);
     }
 }
