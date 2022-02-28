b80ecfc55c7f8793ffdc6b5367d11ce05d48e654
renaruna <79509944+renaruna@users.noreply.github.com>
2022-03-01 00:48:44 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-function late(memID) {
-    document.getElementById("sample").innerHTML = memID+"!";
+var times = 0;
+var rpt = document.getElementById("repeat-images");
+var dftr = rpt.src;
+function repeater(){
+    times++;
+    if(notr % 2 !== 0) {
+        rpt.src = rpt.dataset.secondImage;
+    } else {
+        rpt.src = dftr;
+    }
 }
-late(memID);
