48ea5be04d53583446aadc34921d26bd2c49c058
renaruna <79509944+renaruna@users.noreply.github.com>
2022-03-01 02:14:39 +0900

    Update test.js

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ var $home = $('.rpt');
 function repeater(){
     times++;
     if(times % 2 !== 0) {
-         $home.animate({'width':'50px'},500); 
+         $home.animate({'marginLeft':'0'},500); 
     } else {
-         $home.animate({'width':'100px'},500);
+         $home.animate({'marginLeft':'100px'},500);
     }
 }
